01f125a9a6eb7e579f403c30535f0e6e059cbae4
root <root@DESKTOP-1PF4AT3.localdomain>
2022-10-18 16:32:30 +0900

    echo

--- echo_server.rs
+++ echo_server.rs
@@ -1,27 +1,23 @@
-use std::{
-    error::Error
-};
-use log::{warn};
-use env_logger::Env;
 use async_std::{
-    task,
-    io::{
-        BufReader,
-        BufWriter,
-    },
+    io::{BufReader, BufWriter},
     net::TcpListner,
+    task,
 };
-use futures::{
-    prelude::*,
-    AsyncRead,
-    AsyncWrite,
-}
+use env_logger::Env;
+use futures::{prelude::*, AsyncRead, AsyncWrite};
+use log::warn;
+use std::error::Error;
+use std::time::Duration;
+use oc_http::{
+    cookies::{Cookies, Cookie},
+};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let listner = TcpListner::bind("127.0.0.1:8080").await?;
+    let _local_addr = listner.local_addr()?;
     let mut incoming = listner.incoming();
     while let Some(stream) = incoming.next().await {
         if let Ok(stream) = stream {
@@ -31,31 +27,75 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn handle_request<S>(stream: S)
-where S: AsyncRead + AsyncWrite + Clone + Unpin
+async fn handle_request<S>(socket: S)
+where
+    S: AsyncRead + AsyncWrite + Clone + Unpin
 {
-    let mut reader = BufReader::new(stream.clone());
-    let mut writer = BufWriter::new(stream);
+    let mut reader = BufReader::new(socket.clone());
+    let mut writer = BufWriter::new(socket);
     let mut buf = vec![0; 65536];
-    match oc_http::http(&mut reader, &mut buf).await {
+    let request = match oc_http::http(&mut reader, &mut buf).await {
         Ok(req) => req,
         Err(err) => {
             warn!("Error {}", err);
             return;
-        },
-    };
-    oc_http::respond(&mut writer, oc_http::Response{
-        code: 200,
-        reason: "OK",
-        headers: vec!(),
-    }).await.unwrap();
-    writer.write(b"
-<html>
-    <body>
-        <h1>Hello World</h1>
-    </body>
-</html>
-    ").await.unwrap();
+        }
+    let mut cookies = Cookies:new(&request);
+    if request.path == "/echo" && request.method == "GET" {
+        get_echo(&mut writer).await;
+    } else if request.path == "/echo" && request.method == "POST" {
+        post_echo(&mut reader, &mut writer).await;
+        if let Some(_c) = cookies.get("Who") {
+            writer.write(format!("You are a fool of a took!").as_bytes()).await.unwrap();
+        }
+    } else {
+        let mut res = oc_http::Response {
+            code: 404,
+            reason: "NOT FOUND",
+            headers: vec!(),
+        }
+        cookies.add_cookie(Cookie::new("Who", "You fool!"));
+        cookies.write_cookies(&mut res);
+        oc_http::respond(&mut writer, res).await.unwrap();
+    }
     writer.flush().await.unwrap();
 }
 
+async fn get_echo<S>(mut stream: &mut S)
+where S: AsyncWrite + Unpin
+{
+    oc_http::respond(&mut stream, oc_http::Response {
+            code: 200,
+            reason: "OK",
+            headers: vec![],
+    }).await.unwrap();
+    stream.write(b"
+<html>
+    <body>
+        <form method=\"POST\">
+            <input name=\"input\"></input>
+            <input type=\"submit\"></input>
+        </form>
+    </body>
+</html>
+    ").await.unwrap();
+}
+
+async fn post_echo<W, R>(reader: &mut R, mut writer: &mut W)
+where W: AsyncWrite + Unpin,
+      R: AsyncRead + Unpin,
+{
+    oc_http::respond(&mut stream, oc_http::Response {
+            code: 200,
+            reason: "OK",
+            headers: vec![],
+    }).await.unwrap();
+    let mut buf = vec![0; 10];
+    while let Ok(Ok(count)) = async_std::future::timeout(Duration::from_millis(10), reader.read(&mut buf)).await {
+        if count == 0 {
+            break;
+        }
+        writer.write_all(&buf[..count]).await.unwrap();
+        writer.flush().await.unwrap();
+    }
+}
